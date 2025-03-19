2cef91658f7a2c792ba5973e758245f80b9e2e5b
MuhammadUsamaAwan <underusama@gmail.com>
2025-03-19 11:11:44 +0500

    update schema

--- src/sanity/schemaTypes/page.ts
+++ src/sanity/schemaTypes/page.ts
@@ -6,20 +6,21 @@ export const pageType = defineType({
   type: 'document',
   fields: [
     defineField({
-      name: 'seo',
-      title: 'SEO Settings',
-      type: 'seo',
-    }),
-    defineField({
       name: 'slug',
       type: 'slug',
       description: 'The URL path for this page (e.g. /about).',
-      options: { source: 'title' },
       validation: rule => rule.required(),
+    }),
+    defineField({
+      name: 'seo',
+      title: 'SEO Settings',
+      description: 'SEO settings for this page.',
+      type: 'seo',
     }),
     defineField({
       name: 'sections',
       title: 'Sections',
+      description: 'Sections for this page.',
       type: 'array',
       of: [{ type: 'heroSection' }],
     }),
